c828b8a9ac119e98223974b3a021e75a47cbc456
Corneliu Voiculescu <corneliu@voiculescu.org>
2024-12-16 20:23:04 +0200

    Added maven-surefire-plugin and maven-jar-plugin

---
+++ common-maven-plugins/src/test/java/org/voiculescu/MainTest.java
@@ -0,0 +1,17 @@
+package org.voiculescu;
+
+import org.junit.jupiter.api.Test;
+
+
+class MainTest {
+
+    @Test
+    void testHello() {
+        System.out.println("TEST-Hello");
+    }
+
+    @Test
+    void testWorld() {
+        System.out.println("TEST-World");
+    }
+}
