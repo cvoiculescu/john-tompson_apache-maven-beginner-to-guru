3b7d4d6e12852de0b3b91802ccd2174d7e33903d
Corneliu Voiculescu <corneliu@voiculescu.org>
2024-12-17 10:19:11 +0200

    added lombok plugin

---
+++ generating-source-with-maven/src/main/java/org/voiculescu/domain/Person.java
@@ -0,0 +1,16 @@
+package org.voiculescu.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+}
