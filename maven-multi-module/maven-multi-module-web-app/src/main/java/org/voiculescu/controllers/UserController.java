6c9d4b0566211402131e71bf6c5efac990227b90
Corneliu Voiculescu <corneliu.voiculescu@lseg.com>
2024-12-19 15:37:44 +0200

    maven-multi-model: dependencies between modules

---
+++ maven-multi-module/maven-multi-module-web-app/src/main/java/org/voiculescu/controllers/UserController.java
@@ -0,0 +1,12 @@
+package org.voiculescu.controllers;
+
+import org.voiculescu.domain.UserCommand;
+import org.voiculescu.entities.User;
+
+public class UserController {
+
+    User saveUser(UserCommand command){
+        return new User();
+    }
+
+}
