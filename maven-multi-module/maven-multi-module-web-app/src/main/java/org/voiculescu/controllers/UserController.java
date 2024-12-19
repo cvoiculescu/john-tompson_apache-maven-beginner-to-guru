d20a8a5e4d2a5d6a4f26f154e1f2cbb05243301f
Corneliu Voiculescu <corneliu.voiculescu@lseg.com>
2024-12-19 16:35:42 +0200

    maven-multi-model: added mapper in web-app

--- maven-multi-module/maven-multi-module-web-app/src/main/java/org/voiculescu/controllers/UserController.java
+++ maven-multi-module/maven-multi-module-web-app/src/main/java/org/voiculescu/controllers/UserController.java
@@ -2,11 +2,15 @@ package org.voiculescu.controllers;
 
 import org.voiculescu.domain.UserCommand;
 import org.voiculescu.entities.User;
+import org.voiculescu.mapper.UserMapper;
+import org.voiculescu.mapper.UserMapperImpl;
 
 public class UserController {
 
-    User saveUser(UserCommand command){
-        return new User();
+    private final UserMapper userMapper = new UserMapperImpl();
+
+    User saveUser(UserCommand command) {
+        return userMapper.userCommandToUser(command);
     }
 
 }
