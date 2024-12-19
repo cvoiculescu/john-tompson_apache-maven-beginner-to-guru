cc6fabe072dd550d23d2527142f91671a045154b
Corneliu Voiculescu <corneliu.voiculescu@lseg.com>
2024-12-19 13:47:56 +0200

    maven-multi-model: adding first module for jpa

---
+++ maven-multi-module/maven-multi-module-jpa-entities/src/main/java/org/voiculescu/entities/User.java
@@ -0,0 +1,22 @@
+package org.voiculescu.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+}
